4f4fae40e1caf4fbb88ce535a10876e47dbbfb07
Jorge Galindo <jorge.galindo@wizeline.com>
2024-01-20 14:36:00 -0600

    dirty mvp

--- src/components/card.ts
+++ src/components/card.ts
@@ -1,8 +1,14 @@
 import cardStyles from './card.css?inline'
 
 export class CardRecipe extends HTMLElement {
+  private ID: number = 0
+  static get observedAttributes() {
+    return ['title', 'description']
+  }
+
   constructor() {
     super()
+    this.ID = 0
   }
 
   connectedCallback() {
@@ -10,20 +16,66 @@ export class CardRecipe extends HTMLElement {
     const styles = document.createElement('style')
     shadow.appendChild(styles)
 
+    const idLabel = document.createElement('span')
+    idLabel.textContent = this.ID.toString()
+
     const header = document.createElement('h2')
     header.textContent = this.getAttribute('title') || 'title'
 
     const description = document.createElement('p')
     description.textContent = this.getAttribute('description') || 'description'
 
+    const controls = document.createElement('div')
+    const updateBtn = document.createElement('button')
+    const deleteBtn = document.createElement('button')
+    updateBtn.textContent = 'update'
+    deleteBtn.textContent = 'delete'
+
+    updateBtn.addEventListener('click', () => {
+      this.update(header.textContent!, description.textContent!)
+    })
+
+    deleteBtn.addEventListener('click', () => {
+      const id = this.getAttribute('id')
+      const cardToRemove = document.querySelector(`#${id}`)
+      cardToRemove?.remove()
+    })
+
+    controls.appendChild(updateBtn)
+    controls.appendChild(deleteBtn)
+
     const container = document.createElement('div')
     container.className = 'card'
+    container.appendChild(idLabel)
     container.appendChild(header)
     container.appendChild(description)
+    container.appendChild(controls)
 
     shadow.appendChild(container);
 
     styles.textContent = cardStyles
+  }
+
+  update(title: string, description: string) {
+    const modal = document.createElement('modal-form-recipe')
+    modal.setAttribute('title', title)
+    modal.setAttribute('description', description)
+    const id = this.getAttribute('id')!
+    modal.setAttribute('id', id)
+    document.querySelector('.container')!.appendChild(modal)
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    switch (name) {
+      case 'title':
+        const titleElement = this.shadowRoot?.querySelector('h2')
+        if (titleElement) titleElement.textContent = newValue
+        break
+      case 'description':
+        const descriptionElement = this.shadowRoot?.querySelector('p')
+        if (descriptionElement) descriptionElement.textContent = newValue
+        break
+    }
   }
 }
 
--- src/components/modal-form-recipe.ts
+++ src/components/modal-form-recipe.ts
@@ -10,7 +10,7 @@ class ModalFormRecipe extends HTMLElement {
     shadow.innerHTML = `
       <div class="modal-form-recipe">
         <div class="form">
-          <h3>New Recipe</h3>
+          <h3>New Recipe - ${this.getAttribute('id')}</h3>
           
           <form id="form-recipe">
             <label for="input-title">Title</label>
@@ -19,7 +19,8 @@ class ModalFormRecipe extends HTMLElement {
             <label for="input-description">Description</label>
             <input type="text" id="input-description" />
 
-            <button type="submit">Create</button>
+            <button id="btn-cancel">Cancel</button>
+            <button id="btn-submit" type="submit">Create</button>
           </form>
         </div>
       </div>
@@ -32,18 +33,58 @@ class ModalFormRecipe extends HTMLElement {
   }
 
   connectedCallback() {
+    const inputTitle = this.shadow.querySelector('#input-title') as HTMLInputElement
+    const titleVal = this.getAttribute('title')
+    if (titleVal) {
+      inputTitle.value = titleVal
+    }
+
+    const descriptionInput = this.shadow.querySelector('#input-description') as HTMLInputElement
+    const descriptionVal = this.getAttribute('description')
+    if (descriptionVal) {
+      descriptionInput.value = descriptionVal
+    }
+
+    const submitBtn = this.shadow.querySelector('#btn-submit') as HTMLButtonElement
+    submitBtn.disabled = true
+    const btnCancel = this.shadow.querySelector('#btn-cancel') as HTMLInputElement
+
     // # add handlers
     const recipesContainer = document.querySelector('#recipes')
     this.shadow.querySelector('#form-recipe')?.addEventListener('submit', event => {
       event.preventDefault()
+      const recipeID = this.getAttribute('id')
+      if (recipeID) {
+        const recipe = document.querySelector(`card-recipe[id="${recipeID}"]`)!
+        recipe.setAttribute('title', inputTitle.value)
+        recipe.setAttribute('description', descriptionInput.value)
+        this.closeModal()
+        return
+      }
       const newRecipe = document.createElement('card-recipe')
-      const inputTitle = this.shadow.querySelector('#input-title') as HTMLInputElement
       newRecipe.setAttribute('title', inputTitle.value)
-
-      const descriptionInput = this.shadow.querySelector('#input-description') as HTMLInputElement
       newRecipe.setAttribute('description', descriptionInput.value)
+      newRecipe.setAttribute('id', 'id' + this.getAttribute('nextID') || '0')
+
       recipesContainer?.appendChild(newRecipe)
+      this.closeModal()
     })
+
+    btnCancel.addEventListener('click', (event: MouseEvent) => {
+      event.preventDefault()
+      this.closeModal()
+    })
+
+    const checkInput = () => {
+      submitBtn.disabled = !inputTitle.value || !descriptionInput.value
+    }
+    inputTitle.addEventListener('input', checkInput)
+    descriptionInput.addEventListener('input', checkInput)
+  }
+
+  closeModal() {
+    const modal = this.shadow.querySelector('.modal-form-recipe')!
+    modal.remove()
   }
 }
 
